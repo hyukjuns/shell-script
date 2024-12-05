4039b2a0df483c567b5fd209908ca313c4e895fc
hyukjun <hyukjuner@gmail.com>
2024-12-05 11:38:02 +0900

    update tagging sh

--- azure_tagging_system/eventgrid.sh
+++ azure_tagging_system/eventgrid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a SRC_SUBSCRIPTION_IDS=("SUBS_ID" "SUBS_ID")
+declare -a SRC_SUBSCRIPTION_IDS=("")
 
 EVENTGRID_NAME_01=""
 EVENTGRID_NAME_02=""
@@ -25,30 +25,30 @@ do
     --included-event-types Microsoft.Resources.ResourceWriteSuccess \
     --advanced-filter data.authorization.action StringNotContains Microsoft.Resources/tags/write \
     --advanced-filter data.operationName StringContains \
+    Microsoft.Network/trafficmanagerprofiles/write \
+    Microsoft.network/loadBalancers/write \
+    Microsoft.Network/applicationgateways/write \
+    Microsoft.Network/ApplicationGatewayWebApplicationFirewallPolicies \
+    microsoft.servicenetworking/trafficcontrollers \
+    microsoft.containerservice/managedclusters \
+    Microsoft.ContainerRegistry/registries \
+    Microsoft.DocumentDb/databaseAccounts \
+    Microsoft.ManagedIdentity/userAssignedIdentities \
+    Microsoft.Kubernetes/connectedClusters \
+    Microsoft.HybridCompute/machines \
+    microsoft.operationalinsights/workspaces \
+    microsoft.network/frontdoors \
+    microsoft.network/expressroutecircuits \
+    microsoft.network/vpngateways \
+    microsoft.network/virtualwans \
     Microsoft.Compute/disks/write \
     Microsoft.Compute/virtualMachines/write \
-    Microsoft.insights/metricalerts/write \
     Microsoft.KeyVault/vaults/write \
     Microsoft.RecoveryServices/vaults/write \
     Microsoft.Storage/storageAccounts/write \
     Microsoft.Compute/snapshots/write \
-    Microsoft.Datafactory/Factories/write \
-    Microsoft.Network/NetworkSecurityGroups/write  \
-    Microsoft.Network/privateDnsZones/write \
-    Microsoft.Network/privateEndpoints/write \
-    Microsoft.Network/publicIPAddresses/write \
-    Microsoft.Sql/servers/write \
-    Microsoft.Compute/images/write \
-    Microsoft.ContainerRegistry/registries/write \
-    Microsoft.Compute/virtualMachineScaleSets/write \
-    Microsoft.network/loadBalancers/write \
-    Microsoft.Network/networkWatchers/write \
-    Microsoft.operationalInsights/workspaces/write \
-    Microsoft.Network/networkInterfaces/write \
-    Microsoft.portal/dashboards/write \
-    Microsoft.web/serverFarms/write \
-    Microsoft.web/connections/write \
-    Microsoft.automation/automationAccounts/runbooks/write
+    Microsoft.Network/NetworkSecurityGroups/write
+    
 
     echo "--------------------------------------------------------------------------------"
     printf '%40s\n' "Creating Event Grid and Advanced Filter 02"
@@ -63,20 +63,27 @@ do
     Microsoft.web/sites/write \
     Microsoft.automation/automationAccounts/write \
     Microsoft.certificateRegistration/certificateOrders/write \
-    Microsoft.cognitiveServices/accounts/write \
     Microsoft.operationalInsights/queryPacks/write \
-    Microsoft.batch/batchAccounts/write \
     Microsoft.logic/workflows/write \
     Microsoft.insights/components/write \
-    Microsoft.operationsManagement/solutions/write \
     Microsoft.network/virtualNetworks/write \
     Microsoft.DataProtection/BackupVaults/write \
-    Microsoft.Compute/sshPublicKeys/write \
     Microsoft.DBforPostgreSQL/servers/write \
     Microsoft.Network/localnetworkgateways/write \
     Microsoft.Network/virtualnetworkgateways/write \
     Microsoft.Network/natgateways/write \
-    Microsoft.Network/applicationgateways/write
+    Microsoft.Network/applicationgateways/write \
+    Microsoft.Network/privateDnsZones/write \
+    Microsoft.Network/privateEndpoints/write \
+    Microsoft.Network/publicIPAddresses/write \
+    Microsoft.Sql/servers/write \
+    Microsoft.Compute/images/write \
+    Microsoft.ContainerRegistry/registries/write \
+    Microsoft.Compute/virtualMachineScaleSets/write \
+    Microsoft.Network/networkWatchers/write \
+    Microsoft.operationalInsights/workspaces/write \
+    Microsoft.Network/networkInterfaces/write \
+    Microsoft.web/serverFarms/write
 
     echo "--------------------------------------------------------------------------------"
     printf '%40s\n' "Subscription: $sub Completed."
